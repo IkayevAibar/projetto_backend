c527be02be8f67cba6cc8a488209cff2ccee11ee
Aibar Ikayev <a.ikayev@thefactory.kz>
2023-04-19 10:25:29 +0600

    PRJ-10; added user viewset and user serializer

--- projetto/service/views.py
+++ projetto/service/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets, permissions, status
+from rest_framework.permissions import IsAuthenticated
+
+from residence.models import User
+from .serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = User.objects.all().order_by('-date_joined')
-    permission_classes = [permissions.IsAuthenticated]
-    
-    def get_serializer_class(self):
-        if self.action == 'create':
-            return UserRegistrationSerializer
-        return UserSerializer
-    
-    def get_queryset(self):
-        return self.queryset
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data.get('first_name', ''),
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
