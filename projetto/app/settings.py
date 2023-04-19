e1e28d8e1c6fa9d0a5a4b98fc86fa9a64c8bc230
Aibar Ikayev <a.ikayev@thefactory.kz>
2023-04-19 14:09:33 +0600

    PRJ-3; updated requirements file and .env

--- projetto/app/settings.py
+++ projetto/app/settings.py
@@ -1,7 +1,11 @@
 
 from datetime import timedelta
-import os
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+# Initialise environment variables
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -11,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-46no7jqox^op**-*+im()0s67_5y@vnf=1lym0v29r3(1n^ns3'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
