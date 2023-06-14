00fa54a4c4c84aafad8635d06c1fc271897ed957
Aibar Ikayev <a.ikayev@thefactory.kz>
2023-06-14 15:32:36 +0600

    settings update

--- projetto/app/settings.py
+++ projetto/app/settings.py
@@ -146,7 +146,7 @@ AUTH_USER_MODEL = 'service.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
