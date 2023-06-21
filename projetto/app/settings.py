a69a5a37893b4f61a69b22c53be180ec210db2f3
Aibar Ikayev <a.ikayev@thefactory.kz>
2023-06-21 12:55:35 +0600

    drf settings little changes

--- projetto/app/settings.py
+++ projetto/app/settings.py
@@ -148,7 +148,7 @@ AUTH_USER_MODEL = 'service.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
@@ -206,6 +206,7 @@ SWAGGER_SETTINGS = {
         }
     },
     'DOC_EXPANSION': 'none',
+    'OPERATIONS_SORTER': 'method',
 
 }
 
